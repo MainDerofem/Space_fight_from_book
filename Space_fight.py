e0e7aa9f2bd7f0c14a11dee0ac727660807be8ef
MainDerofem <mainderofem@gmail.com>
2021-09-09 21:04:21 +0300

    Add color to screen. Moved line "pygame.display.flip()" to right place

--- Space_fight.py
+++ Space_fight.py
@@ -1,31 +1,30 @@
 import sys
 import pygame
 
+
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
+
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы."""
         pygame.init()
 
-        self.screen = pygame.display.set_mode((800,600))
+        self.screen = pygame.display.set_mode((800, 600))
         pygame.display.set_caption("Alien Invasion")
+        self.bg_color = (230, 230, 230)  # Назначение цвета фона.
 
     def run_game(self):
         """Запуск основного цикла игры"""
-        while True:                    # Отслеживание событий клавиатуры и мыши.
+        while True:  # Отслеживание событий клавиатуры и мыши.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            self.screen.fill(self.bg_color)
 
-        pygame.display.flip()
+            pygame.display.flip()
+
 
 if __name__ == '__main__':
- # Создание экземпляра и запуск игры.
- ai = AlienInvasion()
- ai.run_game()
-
-
-
-
-
-
+    # Создание экземпляра и запуск игры.
+    ai = AlienInvasion()
+    ai.run_game()
