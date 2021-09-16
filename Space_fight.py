647c1cb3d956951e9d468cbd610eb7061c465fcd
MainDerofem <mainderofem@gmail.com>
2021-09-16 20:02:03 +0300

    First alien on the screen

--- Space_fight.py
+++ Space_fight.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 # Формат PNG использовать где много однотонных областей, где потери не допустимы (тексты). Альфа канал для прозрачного фона.
 # Формат JPEG если фотореалистичные изображения.
@@ -16,11 +17,13 @@ class AlienInvasion():
         self.screen = pygame.display.set_mode(
             (self.settings.screen_height, self.settings.screen_height))
 
-        pygame.display.set_caption("Alien Invasion")
+        pygame.display.set_caption("Чертовы инопланетяне")
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group() #Воспользуемся группой для прорисовки снарядов на экране при каждом
                                              #проходе основного цикла и обновления текущей позиции каждого снаряда.
+        self.aliens = pygame.sprite.Group()
+        self._create_fleet()
 
         clock = pygame.time.Clock()
         FPS = 60
@@ -28,16 +31,10 @@ class AlienInvasion():
         """Запуск основного цикла игры"""
         while True:  # Отслеживание событий клавиатуры и мыши.
             self._check_events()  # При каждом проходе цикла, перерисовывается экран.
-            self.bullets.update() #Вызывает bullet.update() для каждого снаряда, включенного в группу bullets.
             self.ship.update()
-
-            for bullet in self.bullets.copy(): # Удаление снарядов, вышедших за край экрана.
-                if bullet.rect.bottom <= 0: #Если снаряд пересек границу, он удаляется из bullets.
-                    self.bullets.remove(bullet)
-            #print(len(self.bullets)) #Cообщает, сколько снарядов сейчас существует в игре; по выведенному значению
-                                     #можно убедиться в том, что снаряды действительно удаляются при достижении
-                                     #верхнего края экрана.
             self._update_screen()
+            self._update_bullets()
+
         clock.tick(FPS)
 
     def _check_events(self):  #Обнаруживает важные события: нажатия и отпускания клавиш.
@@ -77,8 +74,28 @@ class AlienInvasion():
 
     def _fire_bullet(self):
         """Создание нового снаряда и включение его в группу bullets."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Обновляет позиции снарядов и уничтожает старые снаряды."""
+        # Обновление позиций снарядов.
+        self.bullets.update()  #Вызывает bullet.update() для каждого снаряда, включенного в группу bullets.
+        # Удаление снарядов, вышедших за край экрана.
+        for bullet in self.bullets.copy(): # Удаление снарядов, вышедших за край экрана.
+            if bullet.rect.bottom <= 0: #Если снаряд пересек границу, он удаляется из bullets.
+                self.bullets.remove(bullet)
+            # print(len(self.bullets)) #Cообщает, сколько снарядов сейчас существует в игре; по выведенному значению
+            # можно убедиться в том, что снаряды действительно удаляются при достижении
+            # верхнего края экрана.
+
+    def _create_fleet(self):
+        """Создание флота вторжения."""
+        # Создание пришельца.
+        alien = Alien(self)
+        self.aliens.add(alien)
+
 
     def _update_screen(self): #Перерисовывает экран при каждом проходе основного цикла.
         """Обновляет изображения на экране и отображает новый экран."""
@@ -86,6 +103,7 @@ class AlienInvasion():
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
 
         pygame.display.flip()
 
---
+++ alien.py
@@ -0,0 +1,27 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
+    """Класс, представляющий одного пришельца."""
+
+    def __init__(self, ai_game):
+        """Инициализирует пришельца и задает его начальную позицию."""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+    # Загрузка изображения пришельца и назначение атрибута rect.
+        self.image = pygame.image.load(r'C:\Users\maind\PycharmProjects\Pictures\Alien_3.png')
+        self.image = pygame.transform.scale(self.image,(60,60))
+        self.image.set_colorkey((255, 255, 255))
+        self.rect = self.image.get_rect()
+        self.rect.top = self.screen_rect.top
+
+    # Каждый новый пришелец появляется в левом верхнем углу экрана.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+# Сохранение точной горизонтальной позиции пришельца.
+        self.x = float(self.rect.x)
+
+    def blite_2(self):
+        self.screen.blit(self.image,self.rect)
